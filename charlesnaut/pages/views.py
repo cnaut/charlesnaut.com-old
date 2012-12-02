dc7ea8417a64f78ad68428b722118b692b87fe3f
Charles Naut <cpn585@gmail.com>
2012-12-01 21:47:09 -0800

    Serve homepage using templates

--- charlesnaut/pages/views.py
+++ charlesnaut/pages/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def home(request):
-    return HttpResponse("Charles Naut's Homepage - Work In Progress")  
+    return render_to_response("pages/home.html")  
