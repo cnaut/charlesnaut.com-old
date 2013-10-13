f319bafcca7b62cde9a47974b9d93984a2fcbd19
Charles Naut <Charles Naut@Charles.(none)>
2013-10-13 00:47:58 -0700

    Make home.html and main.css related to the current homepage

--- charlesnaut/pages/views.py
+++ charlesnaut/pages/views.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def home(request):
-    return render_to_response("pages/home3.html")  
+    return render_to_response("pages/home.html")  
 
 def v1(request):
-    return render_to_response("pages/home.html")  
+    return render_to_response("pages/home4.html")  
 
 def v2(request):
     return render_to_response("pages/home2.html")  
